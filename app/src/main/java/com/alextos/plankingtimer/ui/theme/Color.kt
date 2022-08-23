55f61f940d2d4229b5fc2e2f11ff6037786852bc
aleks100s <aleksandr100s@yandex.ru>
2022-08-23 17:18:29 +0300

    Clean project

--- app/src/main/java/com/alextos/plankingtimer/ui/theme/Color.kt
+++ app/src/main/java/com/alextos/plankingtimer/ui/theme/Color.kt
@@ -4,6 +4,10 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 
+val TimerBarColor = Color(0xFF4BE952)
+val TimerBackgroundColor = Color(0xFF272A2C)
+val AccentColor = Color(0xFF03A9F4)
+
 val darkColorScheme = darkColorScheme(
     surface = Color.Black,
     onSurface = Color.White
