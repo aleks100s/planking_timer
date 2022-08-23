bce91eece6bc5803fb35a2906700ea96e62ed4af
aleks100s <aleksandr100s@yandex.ru>
2022-08-23 23:12:22 +0300

    Setup Crashlytics

--- app/src/main/java/com/alextos/plankingtimer/presentation/MainActivity.kt
+++ app/src/main/java/com/alextos/plankingtimer/presentation/MainActivity.kt
@@ -3,9 +3,11 @@ package com.alextos.plankingtimer.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,10 +26,18 @@ class MainActivity : ComponentActivity() {
                         .fillMaxHeight()
                         .fillMaxWidth()
                 ) {
-                    Text(
-                        modifier = Modifier.padding(16.dp),
-                        text = "We are now using Material Design 3!",
-                    )
+                    Column {
+                        Text(
+                            modifier = Modifier.padding(16.dp),
+                            text = "We are now using Material Design 3!",
+                        )
+
+                        Button(onClick = {
+                            throw RuntimeException("Test Crash")
+                        }) {
+                            Text(text = "Crash")
+                        }
+                    }
                 }
             }
         }
